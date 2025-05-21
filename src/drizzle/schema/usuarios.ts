51273a211816d39d9ccbe78f6a2afa77ce4845d4
JaviF <efiallos2433@uta.edu.ec>
2025-05-21 15:24:04 -0500

    comentarios y desencriptacion

--- src/drizzle/schema/usuarios.ts
+++ src/drizzle/schema/usuarios.ts
@@ -9,7 +9,7 @@ export const usuarios = pgTable("usuarios", {
   cedula: varchar("cedula", { length: 20 }).notNull().unique(),
   telefono: varchar("telefono", { length: 20 }),
   activo: boolean("activo").default(true),
-  rol: integer("rol").notNull(),
+  rol: integer("rol").notNull(), //1=Admin, 2=Oficinista, 3=Chofer, 4=Cliente, 5=Superadmin
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
   deletedAt: timestamp("deleted_at"),
