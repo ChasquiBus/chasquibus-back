1f6dc103b5e658d1b6479f615945f35965cb19ad
JaviF <efiallos2433@uta.edu.ec>
2025-05-20 23:10:56 -0500

    Se agregaron campos en la tabla Cooperativas

---
+++ src/drizzle/migrations/0006_cultured_fat_cobra.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "cooperativa_transporte" ADD COLUMN "ruc" varchar(20) NOT NULL;--> statement-breakpoint
+ALTER TABLE "cooperativa_transporte" ADD COLUMN "sitio_web" varchar(255);--> statement-breakpoint
+ALTER TABLE "cooperativa_transporte" ADD COLUMN "direccion" varchar(255);
