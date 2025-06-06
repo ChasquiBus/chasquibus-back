cf0d3ef5469076f1ff4d9ab8552c5cebb2cf2ef8
JaviF <efiallos2433@uta.edu.ec>
2025-06-06 12:02:59 -0500

    Se cambio dos atributos de chofer en la base a not null, se agrego un campo para asociar a un cliente a una Cooperativa

--- src/drizzle/schema/choferes.ts
+++ src/drizzle/schema/choferes.ts
@@ -1,11 +1,13 @@
 import {  date, integer, pgTable, serial, timestamp, varchar } from "drizzle-orm/pg-core";
 import { usuarios } from "./usuarios";
+import { cooperativaTransporte } from "./cooperativa-transporte";
 
 export const choferes = pgTable("choferes", {
   id: serial("id").primaryKey(),
   usuarioId: integer("usuario_id").notNull().references(() => usuarios.id),
-  numeroLicencia: varchar("numero_licencia", { length: 100 }),
-  tipoLicencia: varchar("tipo_licencia", { length: 100 }),
+  numeroLicencia: varchar("numero_licencia", { length: 100 }).notNull(),
+  tipoLicencia: varchar("tipo_licencia", { length: 100 }).notNull(),
   tipoSangre: varchar("tipo_sangre", { length: 100 }),
   fechaNacimiento: date("fecha_nacimiento"),
+  cooperativaTransporteId: integer("cooperativa_transporte_id").notNull().references(() => cooperativaTransporte.id),
 });
