640cf0a6a12572de847d04311f043b15a0754f4f
JaviF <efiallos2433@uta.edu.ec>
2025-06-06 19:20:18 -0500

    se implemento serviocios de usuario en cliente

--- src/clientes/clientes.module.ts
+++ src/clientes/clientes.module.ts
@@ -3,12 +3,10 @@ import { ClientesService } from './clientes.service';
 import { AuthModule } from 'auth/auth.module';
 import { ClientesController } from './clientes.controller';
 import { ClientesRegistroController } from './cliente-registro.controller';
+import { UsuariosModule } from 'usuarios/usuario.module';
 
 @Module({
-  providers: [ClientesService]
-})
-@Module({
-  imports: [AuthModule],  
+  imports: [AuthModule, UsuariosModule],  
   controllers: [ClientesController, ClientesRegistroController], 
   providers: [ClientesService],     
   exports: [ClientesService],        
