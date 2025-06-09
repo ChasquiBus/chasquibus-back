22a5796b49d791a6607c804cfb5959ba97b2eb1d
JaviF <efiallos2433@uta.edu.ec>
2025-06-08 20:49:14 -0500

    actuizacion del index

--- src/drizzle/schema/index.ts
+++ src/drizzle/schema/index.ts
@@ -1,13 +1,47 @@
-import { choferes } from './choferes';
-import { clientes } from './clientes';
-import { cooperativaTransporte } from './cooperativa-transporte';
-import { usuarioCooperativa } from './usuario-cooperativa';
 import { usuarios } from './usuarios';
+import { clientes } from './clientes';
+import { usuarioCooperativa } from './usuario-cooperativa';
+import { cooperativaTransporte } from './cooperativa-transporte';
+import { choferes } from './choferes';
+import { buses } from './bus';
+import { configuracionAsientos } from './configuracion-asientos';
+
+import { resolucionesAnt } from './resoluciones-ant';
+import { ciudades } from './ciudades';
+import { paradas } from './paradas';
+import { rutas } from './rutas';
+import { rutaParada } from './ruta-parada';
+import { precios } from './precios';
+import { frecuencias } from './frecuencias';
+import { hojaTrabajo } from './hoja-trabajo';
+import { horarios } from './horarios';
+import { descuentos } from './descuentos';
+import { metodosPago } from './metodos-pago';
+import { ventas } from './ventas';
+import { boletos } from './boletos';
+import { notificaciones } from './notificaciones';
 
 export const schema = {
   usuarios,
   clientes,
   usuarioCooperativa,
   cooperativaTransporte,
-  choferes
+  choferes,
+  buses,
+  configuracionAsientos,
+
+  resolucionesAnt,
+  ciudades,
+  paradas,
+  rutas,
+  rutaParada,
+  precios,
+  frecuencias,
+  hojaTrabajo,
+  horarios,
+  descuentos,
+  metodosPago,
+  ventas,
+  boletos,
+  notificaciones,
 };
