e6cffc1604091e1464968d6c16b9b440ef094c01
JaviF <efiallos2433@uta.edu.ec>
2025-06-12 15:11:08 -0500

    create resolucion dto

--- src/resoluciones/dto/create-resolucion.dto.ts
+++ src/resoluciones/dto/create-resolucion.dto.ts
@@ -1,16 +1,1 @@
-import { IsDateString, IsOptional, IsString, IsBoolean } from 'class-validator';
-
-export class CreateResolucionAntDto {
-  @IsOptional()
-  @IsString()
-  documentoURL?: string;
-
-  @IsDateString()
-  fechaEmision: string;
-
-  @IsDateString()
-  fechaVencimiento: string;
-
-  @IsBoolean()
-  estado: boolean;
-}
+export class CreateResolucionDto {}
