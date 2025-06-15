c805aa8f837b381c1ae28074c1b4340d40d36515
JaviF <efiallos2433@uta.edu.ec>
2025-06-14 19:09:15 -0500

    Se agrego los campos de nombre y descripcion

--- src/drizzle/schema/resoluciones-ant.ts
+++ src/drizzle/schema/resoluciones-ant.ts
@@ -5,6 +5,8 @@ import { cooperativaTransporte } from "./cooperativa-transporte";
 export const resolucionesAnt = pgTable('resoluciones_ant', {
   id: serial('id').primaryKey(),
   documentoURL: varchar('documentoURL', { length: 150 }),
+  nombre: varchar('nombre', { length: 150 }),
+  descripcion: varchar('descripcion', { length: 150 }),
   fechaEmision: date('fecha_emision'),
   fechaVencimiento: date('fecha_vencimiento'),
   estado: boolean('estado'),
