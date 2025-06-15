a80de4dece84f967542c1f54e75ece99f0bfb330
JaviF <efiallos2433@uta.edu.ec>
2025-06-14 20:48:04 -0500

    agragar id cooperativa en el payload

--- src/auth/interfaces/jwt-payload.interface.ts
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -3,5 +3,6 @@ import { RolUsuario } from "auth/roles.enum";
 export interface JwtPayload{
     sub: number;
     email:string;
-    rol: RolUsuario
+    rol: RolUsuario;
+    cooperativaId?: number | null;
 }
