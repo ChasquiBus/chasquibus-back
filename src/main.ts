766c1c324bbf78cf0213cd71a2ffccd8f3cd17cf
JaviF <efiallos2433@uta.edu.ec>
2025-06-14 20:46:46 -0500

    cambio de cors a la ruta del front

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
