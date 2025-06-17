7644cb68019a2e42f099cf11e248f3fae58ef58d
JaviF <efiallos2433@uta.edu.ec>
2025-06-17 13:09:00 -0500

    Se modifica la tabla hoja de trabajo con las relaciones de las nuevas tablas

--- src/drizzle/schema/hoja-trabajo.ts
+++ src/drizzle/schema/hoja-trabajo.ts
@@ -1,13 +1,16 @@
-// hoja-trabajo.ts
-import { pgTable, serial, integer, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, serial, integer, varchar, date, timestamp } from 'drizzle-orm/pg-core';
 import { buses } from './bus';
 import { choferes } from './choferes';
-import { usuarios } from './usuarios';
+import { frecuenciaDias } from './frecuencia-dia';
 
 export const hojaTrabajo = pgTable('hoja_trabajo', {
   id: serial('id').primaryKey(),
-  busId: integer('bus_id').references(() => buses.id),
-  choferId: integer('chofer_id').references(() => choferes.id),
+  busId: integer('bus_id').notNull().references(() => buses.id),
+  choferId: integer('chofer_id').notNull().references(() => choferes.id),
+  frecDiaId: integer('frec_dia_id').notNull().references(() => frecuenciaDias.id),
   observaciones: varchar('observaciones', { length: 255 }),
-  estado: varchar('estado', { length: 50 }), // activa, inactiva, suspendida
+  estado: varchar('estado', { length: 20 }).notNull(), // Ej: Programado, Suspendido, etc.
+  horaSalidaReal: timestamp('hora_salida_real', { withTimezone: true }),
+  horaLlegadaReal: timestamp('hora_llegada_real', { withTimezone: true }),
+  fechaSalida: date('fecha_salida')
 });
