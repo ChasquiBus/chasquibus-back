6597399df49d3137454e18f4fb4f4a93685307ff
JaviF <efiallos2433@uta.edu.ec>
2025-06-23 13:55:40 -0500

    Se agrego un campo en en la tabla bus enUSo

--- src/frecuencias/dto/create-frecuencia.dto.ts
+++ src/frecuencias/dto/create-frecuencia.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString, IsDateString, Matches } from 'class-validator';
+import { IsNumber, IsString, Matches } from 'class-validator';
 
 export class CreateFrecuenciaDto {
   @ApiProperty({ example: 1, description: 'ID de la ruta a la que pertenece esta frecuencia' })
@@ -13,7 +13,6 @@ export class CreateFrecuenciaDto {
   @ApiProperty({ example: '10:00:00', description: 'Hora programada de llegada en formato HH:mm:ss' })
   @Matches(/^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/, { message: 'La horaLlegadaProg debe tener el formato HH:mm:ss' })
   horaLlegadaProg: string;
-
 
   @ApiProperty({ example: 'activa', description: 'Estado de la frecuencia' })
   @IsString()
