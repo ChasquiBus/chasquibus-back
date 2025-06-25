eb0b4723d95336ee61893aa2d7ddf3e798dcd2fc
JaviF <efiallos2433@uta.edu.ec>
2025-06-25 12:08:26 -0500

    Se dio permisos de lectura a los descuentos

--- src/descuentos/descuentos.service.ts
+++ src/descuentos/descuentos.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
+// descuentos.service.ts
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDescuentoDto } from './dto/create-descuento.dto';
 import { UpdateDescuentoDto } from './dto/update-descuento.dto';
+import { descuentos } from '../drizzle/schema/descuentos';
+import { db } from 'drizzle/database';
+import { eq } from 'drizzle-orm';
+
 
 @Injectable()
 export class DescuentosService {
-  create(createDescuentoDto: CreateDescuentoDto) {
-    return 'This action adds a new descuento';
+  async create(dto: CreateDescuentoDto) {
+    return db.insert(descuentos).values({
+      ...dto,
+      porcentaje: dto.porcentaje.toString(),
+    }).returning();
   }
 
-  findAll() {
-    return `This action returns all descuentos`;
+  async findAll() {
+    return db.select().from(descuentos);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} descuento`;
+  async findOne(id: number) {
+    const result = await db.select().from(descuentos).where(eq(descuentos.id, id));
+    if (!result.length) throw new NotFoundException('Descuento no encontrado');
+    return result[0];
   }
 
-  update(id: number, updateDescuentoDto: UpdateDescuentoDto) {
-    return `This action updates a #${id} descuento`;
+  async update(id: number, dto: UpdateDescuentoDto) {
+    return db.update(descuentos).set({
+      ...dto,
+      porcentaje: dto.porcentaje?.toString(),
+    }).where(eq(descuentos.id, id)).returning();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} descuento`;
+  async remove(id: number) {
+    return db.delete(descuentos).where(eq(descuentos.id, id)).returning();
   }
 }
